5a467d7164aedaa27702a1a382fd10ebfe882d07
Mut1s0 <musyokaallan96@gmail.com>
2023-01-06 10:17:26 +0300

    added functionality for the guest score

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 let homeScore = document.getElementById("home-score")
+let guestScore = document.getElementById("guest-score")
 
 let homePoints = 0
+let guestPoints = 0
 
 function add1() {
-  console.log("clicked")
   homePoints += 1
   homeScore.textContent = homePoints
 }
@@ -16,4 +17,19 @@ function add2() {
 function add3() {
   homePoints += 3
   homeScore.textContent = homePoints
+}
+
+function addOne() {
+  guestPoints += 1
+  guestScore.textContent = guestPoints
+}
+
+function addTwo() {
+  guestPoints += 2
+  guestScore.textContent = guestPoints
+}
+
+function addThree() {
+  guestPoints += 3
+  guestScore.textContent = guestPoints
 }
